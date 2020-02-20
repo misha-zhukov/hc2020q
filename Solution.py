a2817a78583b5ec361227591f26b81b4b9e8a1dd
Misha Zhukov <3v1l91l@gmail.com>
2020-02-20 21:24:39 +0200

    basic alg

--- Solution.py
+++ Solution.py
@@ -26,8 +26,40 @@ def solve_file(filepath):
             lib_book_ids.append(cur_book_ids)
             lib_ids.append(i)
 
-    result = None
-    return result
+        lib_score = []
+        for i in range(0, n_libs):
+            score = sum(map(lambda x: book_scores[x], lib_book_ids[i]))
+            lib_score.append(score * lib_ship_books[i])
+
+        lib_sort_indexes = sorted(
+            range(len(lib_score)), key=lib_score.__getitem__)
+
+        days_left = n_days
+        zipped = zip(lib_score, lib_ids)
+        sort = sorted(zipped, key=lambda x: x[0], reverse=False)
+
+        chosen_lib_id_book_n = []
+        chosen_book_ids = []
+        already_shipped_books = set()
+        while days_left > 0 and len(sort) > 0:
+            lib_id = sort.pop()[1]
+            days_left -= lib_signup_days[lib_id]
+            if days_left <= 0:
+                continue
+            # lib_ship_books[lib_id] = [
+            #     x for x in lib_ship_books[lib_id]]
+            books_will_be_shipped = min(
+                days_left * lib_ship_books[lib_id], lib_n_books[lib_id])
+
+            chosen_book_ids.append(
+                lib_book_ids[lib_id][:books_will_be_shipped])
+            chosen_lib_id_book_n.append([lib_id, books_will_be_shipped])
+            already_shipped_books.update(
+                lib_book_ids[lib_id][:books_will_be_shipped])
+
+    # for day in range(0, n_days):
+
+    return chosen_lib_id_book_n, chosen_book_ids
 
 
 if __name__ == '__main__':
@@ -35,17 +67,25 @@ if __name__ == '__main__':
     problems_dir_path = 'in'
     solutions_dir_path = 'out'
 
-    # problem_files = filesHelper.get_problem_files(problems_dir_path)
-    problem_files = ['a_example.txt']
+    problem_files = filesHelper.get_problem_files(problems_dir_path)
+    # problem_files = ['a_example.txt']
 
     for file in problem_files:
         print(f'Start working on {file}')
         start_time = time.time()
-        result = solve_file(
+        chosen_lib_id_book_n, chosen_book_ids = solve_file(
             path.join(os.getcwd(), problems_dir_path, file))
         with open(path.join(os.getcwd(), solutions_dir_path, file), 'w') as f:
-            f.write(str(len(out)) + '\n')
-            for item in result:
-                pass
+            f.write(str(len(chosen_lib_id_book_n)) + '\n')
+            for i in range(0, len(chosen_lib_id_book_n)):
+                f.write(
+                    str(chosen_lib_id_book_n[i][0]) + ' ' + str(chosen_lib_id_book_n[i][1]) + '\n')
+                for j in range(0, len(chosen_book_ids[i])):
+                    string = str(chosen_book_ids[i][j])
+                    if j == len(chosen_book_ids[i])-1:
+                        string += '\n'
+                    else:
+                        string += ' '
+                    f.write(string)
         print(f'Finished working on {file}')
         print("--- %s seconds ---" % (time.time() - start_time))
